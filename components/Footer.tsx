d20693bcd5fde0b60a5f5ef30e6a66a995c646ef
giannis198 <john_nik@windowslive.com>
2023-09-23 09:42:37 +0300

    Footer Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,49 @@
+import Link from "next/link";
+import { footerLinks } from "@/constants";
+
 const Footer = () => {
   return (
-    <footer className="border-t bg-white">
-      <div className="mx-auto py-10">
-        <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
-        </p>
-        <p className="text-center text-sm text-black">
-          Created by ioannis @2023
-        </p>
+    // <footer className="border-t bg-white">
+    //   <div className="mx-auto py-10">
+    //     <p className="text-center text-xs text-black">
+    //       &copy; 2023 Store, Inc. All rights reserved.
+    //     </p>
+    //     <p className="text-center text-sm text-black">
+    //       Created by ioannis @2023
+    //     </p>
+    //   </div>
+    // </footer>
+    <footer className="text-black-100 mt-5 flex flex-col border-t border-gray-100">
+      <div className="flex flex-wrap justify-between gap-5 px-6 py-10 max-md:flex-col sm:px-16">
+        <div className="flex flex-col items-start justify-start gap-6">
+          STORE LOGO
+          <p className="text-base">
+            STORE 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link key={item.title} href={item.url}>
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-10 flex flex-wrap items-center justify-between border-t border-gray-100 px-6 py-10 sm:px-16">
+        <p className="mr-3">Created by ioannis @2023</p>
+        <p> All Rights Reserved.</p>
+
+        <div className="footer__copyrights-link">
+          <Link href="/">Privacy Policy</Link>
+          <Link href="/">Terms Of Use</Link>
+        </div>
       </div>
     </footer>
   );
---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "About Store", url: "/" },
+      { title: "Affiliate & influencer Program", url: "/" },
+      { title: "Contact us", url: "/" },
+      { title: "Careers", url: "/" },
+      { title: "Press", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Invite a friend", url: "/" },
+    ],
+  },
+  
+  {
+    title: "Socials",
+    links: [
+      { title: "Discord", url: "/" },
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
